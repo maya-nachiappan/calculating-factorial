e8818e060f95c4408fe43de693662f3a07aa95fd
maya-nachiappan <maya.n102304@gmail.com>
2024-09-18 03:06:40 -0700

    Initial Commit

---
+++ CIS25 - assignment 12/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CIS25 - assignment 12
+//
+//  Created by maya nachiappan on 9/18/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
