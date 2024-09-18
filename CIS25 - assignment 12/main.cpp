fa305b0d1492e428d18fd1fc242dea5fefa7c4d5
maya-nachiappan <maya.n102304@gmail.com>
2024-09-18 03:12:59 -0700

    initialized variables, prints factorials and error message for non positive number

--- CIS25 - assignment 12/main.cpp
+++ CIS25 - assignment 12/main.cpp
@@ -1,14 +1,32 @@
 //
-//  main.cpp
 //  CIS25 - assignment 12
 //
 //  Created by maya nachiappan on 9/18/24.
 //
+// a program that prints the factorial of a positive number
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    unsigned long long fact=1;
+    int num;
+    
+    cout << "enter a positive number: " << endl;
+    cin >> num;
+    
+    if (num<0)
+    {
+        cout << "that is not a positive number" << endl;
+        return 1;
+    }
+    
+    for (int i=1; i<=num; ++i)
+    {
+        fact*= i;
+    }
+    
+    cout << "the factorial of the number is: " << fact << endl;
     return 0;
 }
